a4f6f7890b18ce10181b0816a464d7f630326f0e
Tamer <tamersgc@gmail.com>
2023-09-22 13:23:55 -0300

    app front end notes

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,43 @@
+import { RiShutDownLine } from 'react-icons/ri'
+import { Container, Profile, Logout } from './styles'
+import { useAuth } from '../../hooks/auth';
+import { api } from "../../services/api";
+import { useNavigate } from 'react-router-dom';
+
+import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
+
+
+export function Header() {
+  const { signOut, user } = useAuth();
+
+  const navigate = useNavigate();
+
+
+  function handleSignOut(){
+    navigate("/");
+    signOut();
+
+  }
+
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+
+  return (
+    <Container>
+      <Profile to="/profile">
+        <img
+          src={avatarUrl}
+          alt={user.name}
+        />
+
+        <div>
+          <span>Bem-vindo</span>
+          <strong>{user.name}</strong>
+        </div>
+      </Profile>
+
+      <Logout onClick={handleSignOut}>
+        <RiShutDownLine />
+      </Logout>
+    </Container>
+  )
+}
